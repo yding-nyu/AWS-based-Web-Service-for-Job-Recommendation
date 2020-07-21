83067aefd2361ded935cb1de57203a3564972210
Yuan Ding <43052457+yding-nyu@users.noreply.github.com>
2020-07-20 22:30:31 -0400

    Update Login.java

--- src/main/java/rpc/Login.java
+++ src/main/java/rpc/Login.java
@@ -22,7 +22,6 @@ public class Login extends HttpServlet {
      */
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -49,7 +48,6 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		JSONObject input = RpcHelper.readJSONObject(request);
 		String userId = input.getString("user_id");
 		String password = input.getString("password");
